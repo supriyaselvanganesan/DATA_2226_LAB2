bb73634215d9464a22438f3fae95801f6e20e953
Supriya Selvan Ganesan <supriya.selvanganesan@sjsu.edu>
2025-11-24 23:03:58 -0800

    Add files via upload

---
+++ dbt/models/output/stock_price_technical_indicators.sql
@@ -0,0 +1,35 @@
+select
+  symbol,
+  date,
+  close,
+  volume,
+  sma_20,
+  sma_50,
+  sma_200,
+  momentum_10,
+  rsi_14,
+
+  case 
+    when sma_20 > sma_50 then 'Bullish'
+    else 'Bearish'
+  end as regime_20_50,
+
+  case
+    when rsi_14 > 70 then 'Overbought'
+    when rsi_14 < 30 then 'Oversold'
+    else 'Neutral'
+  end as rsi_signal,
+
+  case
+    when momentum_10 > 0 then 'Uptrend'
+    when momentum_10 < 0 then 'Downtrend'
+    else 'No Change'
+  end as momentum_signal,
+
+  case
+    when rsi_14 < 30 and momentum_10 > 0 then 'BUY'      
+    when rsi_14 > 70 and momentum_10 < 0 then 'SELL'     
+    else 'HOLD'
+  end as trade_signal
+
+from {{ ref('stock_transformations') }}
